0432869c6b2df99ce237f80a8d3c423948b8c7e1
Håkan Råberg <hakan.raberg@gmail.com>
2015-12-16 22:25:05 +0000

    Upgrading to Clojure 1.8.0-RC4

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [org.clojure/clojurescript "1.7.189" :scope "provided"]
                  [cljsjs/react "0.14.3-0"]
                  [cljsjs/react-dom "0.14.3-1"]]
